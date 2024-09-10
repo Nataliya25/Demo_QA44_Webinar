0ac5e206d5767d5d0ab3a51373dd56f489127d75
Nataliya25 <bnataliya13@gmail.com>
2024-09-10 12:43:59 +0300

    Project_webinar

---
+++ src/main/java/pages/BasePage.java
@@ -0,0 +1,12 @@
+package pages;
+
+import org.openqa.selenium.WebDriver;
+
+public class BasePage {
+
+    protected static WebDriver driver;
+
+    public static void setDriver(WebDriver wd) {
+        driver = wd;
+    }
+}
